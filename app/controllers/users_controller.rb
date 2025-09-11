835cf455c88e1c869640f9e3b8be38992741943e
Ho Sy Thao <hosithao1622004@gmail.com>
2025-09-11 11:21:22 +0700

    add Mas Assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,36 @@
 class UsersController < ApplicationController
-  def show
+  before_action :load_user, only: %i(show edit update)
+  # GET /users/:id
+  def show; end
+
+  # GET /users/:id/edit
+  def edit; end
+
+  # PATCH /users/:id
+  def update
+    if @user.update(params[:user].permit!)
+      flash[:success] = "Profile updated successfully."
+      redirect_to user_path(@user)
+    else
+      flash.now[:danger] = "Failed to update profile."
+      render :edit
+    end
   end
 
-  def update
+  private
+
+  # def user_params
+  #   # Only allow email and password changes, NOT role
+  #   params.require(:user).permit(:email, :password, :password_confirmation)
+  # end
+
+  def load_user
+    @user = User.find(params[:id])
+    return if @user
+
+    # return if @user == current_user
+
+    flash[:danger] = "User not found."
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :sessions, only: %i(new create destroy)
-  resources :users, only: %i(show update)
+  resources :users, only: %i(show edit update)
   resources :events do
     resources :comments, only: %i(create destroy)
   end
