64840f72ab377a4952f79f9e155dc7127d0d0aa8
Ho Sy Thao <hosithao1622004@gmail.com>
2025-09-12 08:23:55 +0700

    add IDOR

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
-  def show
-  end
+  before_action :load_user, only: :show
+  def show; end
 
-  def update
+  def edit; end
+  private
+  def load_user
+    @user = User.find(params[:id])
+    return if @user
+
+    # return if @user == current_user
+
+    flash[:danger] = "User not found"
+    redirect_to root_path
   end
 end
